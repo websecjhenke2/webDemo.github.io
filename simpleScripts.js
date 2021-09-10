af651160a38e3b88b8694dc824056e0d81e689c3
Justin Henke <57231751+jhenke2@users.noreply.github.com>
2021-09-10 09:55:39 -0400

    Delete simpleScripts.js

--- simpleScripts.js
+++
@@ -1,8 +0,0 @@
-document.querySelector("#messages").innerText = "bananas";
-var i = 0;
-var clickHandler = function(eventObject) {
-    document.querySelector("#counter").innerText = "You have clicked this button " + ++i + " times";
-
-}
-
-document.querySelector("#first_button").addEventListener("click",clickHandler);
